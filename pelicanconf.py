13368f81eb7b7fb6540f5d10e12eb01a20511eb0
B An <BA@Bs-MacBook-Pro.local>
2019-12-06 11:27:18 -0500

    website

---
+++ pelicanconf.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Brandon Anhorn'
+SITENAME = 'BA Blog'
+SITEURL = 'https://brandonanhorn.github.io/Blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
