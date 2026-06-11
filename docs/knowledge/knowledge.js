367649b4f3bb258ee175c68f887c25594c4a42fd
Brandon Anhorn <brandon.anhorn@gmail.com>
2026-06-11 13:15:49 -0600

    tunneling like diglet bro

--- docs/knowledge/knowledge.js
+++ docs/knowledge/knowledge.js
@@ -1,5 +1,5 @@
 (() => {
-  const API_URL = "https://simultaneously-likes-tariff-historic.trycloudflare.com/api/chat";
+  const API_URL = "https://wallet-realm-intelligent-problems.trycloudflare.com/api/chat";
   const MULTIMODAL_API_URL = API_URL.replace(/\/api\/chat\/?$/, "/api/chat-multimodal");
   const FEEDBACK_URL = API_URL.replace(/\/api\/chat\/?$/, "/api/feedback");
   const form = document.querySelector("[data-knowledge-form]");
